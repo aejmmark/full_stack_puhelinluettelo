6517f71472235148d2f5b86f3bd39feb2aaac1e4
aejmmark <aejmelaeus.markus@gmail.com>
2022-01-01 19:02:20 +0200

    Fix index.js not using express.static and restore errors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Person = require('./models/person')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -73,23 +74,23 @@ app.delete('/api/persons/:id', (req, res, next) => {
   .catch(error => next(error))
 })
 
-//const errorHandler = (error, request, response, next) => {
-//  console.error(error.message)
-//
-//  if (error.name === 'CastError') {
-//    return response.status(400).send({ error: 'malformatted id' })
-//  } else if (error.name === 'ValidationError') {
-//    return response.status(400).json({ error: error.message })
-//  }
-//
-//  next(error)
-//}
-//app.use(errorHandler)
-//
-//const unknownEndpoint = (request, response) => {
-//  response.status(404).send({ error: 'unknown endpoint' })
-//}
-//app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
