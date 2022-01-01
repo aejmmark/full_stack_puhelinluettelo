070577a251bf8b87f334e66d87f059a11b754fae
aejmmark <aejmelaeus.markus@gmail.com>
2022-01-01 18:56:31 +0200

    Temporarily disable error handlers

--- index.js
+++ index.js
@@ -73,23 +73,23 @@ app.delete('/api/persons/:id', (req, res, next) => {
   .catch(error => next(error))
 })
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-app.use(errorHandler)
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
+//const errorHandler = (error, request, response, next) => {
+//  console.error(error.message)
+//
+//  if (error.name === 'CastError') {
+//    return response.status(400).send({ error: 'malformatted id' })
+//  } else if (error.name === 'ValidationError') {
+//    return response.status(400).json({ error: error.message })
+//  }
+//
+//  next(error)
+//}
+//app.use(errorHandler)
+//
+//const unknownEndpoint = (request, response) => {
+//  response.status(404).send({ error: 'unknown endpoint' })
+//}
+//app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
